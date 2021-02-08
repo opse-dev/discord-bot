0897c40b7218cf46ab3cbb727a575bf513029df1
Taimoor-Tariq <45585081+ANN0Y1NGHACKER@users.noreply.github.com>
2021-02-08 12:42:49 -0500

    Channel Automations

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,13 @@
 module.exports = (client) => {
 	require('../modules/gameday-channels');
 	console.log(` \nLogged in as ${client.user.tag}!`);
+
+	console.log(new Intl.DateTimeFormat('en', {
+		timeZone: 'America/Toronto',
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric',
+		hour: 'numeric',
+		minute: 'numeric',
+	}).format(new Date()))
 };
--- modules/gameday-channels.js
+++ modules/gameday-channels.js
@@ -1,5 +1,6 @@
 const
     client = global.client,
+    { BOT_TOKEN } = require('../config'),
     axios = require('axios'),
     schedule = require('node-schedule'),
     leagues = ["", "hs", "lol", "ow", "rl"],
@@ -8,10 +9,50 @@ const
 
 let delete_channels = () => {
     return new Promise(resolve => {
-        let prom = client.channels.cache.filter(c => c.parentID == "806861228114837554").map(c => { c.delete(); });
+        let prom = client.channels.cache.filter(c => c.parentID == "806861228114837554").map(async c => { await c.delete(); });
 
         Promise.all(prom).then(() => {resolve()});
     });
+}
+
+let sendMsgNoPings = (channelID, msg) => {
+	return new Promise((resolve, reject) => {
+		axios({
+			method: "POST",
+			url: `https://discord.com/api/channels/${channelID}/messages`,
+			headers: {
+				"Authorization": `Bot ${BOT_TOKEN}`
+			},
+			data: {
+				content: msg,
+				allowed_mentions: {
+					users: [],
+					roles: []
+				}
+			}
+		})
+		.then(() => {resolve()})
+		.catch(e => {
+			console.log(e.response.data);
+			reject(e);
+		});
+	})
+}
+
+let sendDefaultMSG = (cID, game) => {
+    let date = new Date();
+	let week = Math.floor(((date.getDate()-18)+(date.getMonth()+1))/7)+15;
+	let short_month = new Intl.DateTimeFormat('en', { month: 'short' }).format(date);
+	let msg = `
+**Match Date:** ${short_month} ${date.getDate()}, ${date.getFullYear()}
+­**Home Team:** <@&${teamsInfo.filter(t => t.id == game.teamID1)[0].roleID}>
+**Away Team:** <@&${teamsInfo.filter(t => t.id == game.teamID2)[0].roleID}>
+­
+`;
+
+	sendMsgNoPings(cID, msg).then(() => {
+		sendMsgNoPings(cID, `http://api.opsesports.ca/image-generator/create/h2h?game=${leagues[game.leagueID]}&away_logo=${teamsInfo.filter(t => t.id == game.teamID1)[0].imgID}&home_logo=${teamsInfo.filter(t => t.id == game.teamID2)[0].imgID}&line1=${short_month}%20${date.getDate()},%20${date.getFullYear()}&line2=Regular+Season&line3=Week+${week}&download=true`)
+	})
 }
 
 let create_channels = () => {
@@ -47,6 +88,16 @@ let create_channels = () => {
                         allow: ['SEND_MESSAGES', 'READ_MESSAGE_HISTORY'],
                     },
                 ]);
+
+                c.setTopic(`Game Time: ${new Intl.DateTimeFormat('en', {
+                    timeZone: 'America/Toronto',
+                    year: 'numeric',
+                    month: 'short',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric',
+                }).format(new Date(g.date))}`);
+                sendDefaultMSG(c.id, g);
             }).catch(e => console.log(e));
             
             guild.channels.create(`[${leagues[g.leagueID].toLocaleUpperCase()}] ${teamsInfo.filter(t => t.id == g.teamID1)[0].name}`, { type: 'voice' }).then(async c => {
@@ -104,5 +155,5 @@ let run_schedule = () => {
     delete_channels().then(setTimeout(create_channels, 2000))
 }
 
-// schedule.scheduleJob('05 * * * * *', run_schedule);
+// schedule.scheduleJob('35 * * * * *', run_schedule);
 schedule.scheduleJob('00 09 * * *', run_schedule);
